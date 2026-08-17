6b84390e0f8784fb219cfe3078950235de3165fc
alepaulolo-cpu <alepaulolo@gmail.com>
2026-08-17 01:55:45 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://lsfgdpwvmbnwpxdltsey.supabase.co";
+const SUPABASE_KEY = "sb_publishable_Wuq84ptTiE5t-iPEZ2XxCA_TXPUnKXN";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
